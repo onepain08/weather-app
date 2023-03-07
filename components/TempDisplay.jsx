4e4948c7e0051018369cba0119cd7449b3e4ea80
Onepain <onepaintdap@gmail.com>
2023-03-07 19:33:23 +0000

    redeploy

--- components/TempDisplay.jsx
+++ components/TempDisplay.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { useState } from "react";
 import { TbTemperaturePlus, TbTemperatureMinus } from'react-icons/tb'
 import { BsCloudRainHeavyFill, BsSnow } from'react-icons/bs'
 import Image from "next/image";
